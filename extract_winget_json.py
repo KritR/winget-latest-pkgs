66013a751911f470ff950b514297a9124e48de4d
Krithik Rao <krdevmail@gmail.com>
2021-12-08 00:19:40 -0800

    Initial commit to parse winget repo and create json file

---
+++ extract_winget_json.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from packaging.version import parse as parse_version
+import yaml
+import json
+
+all_packages = Path('./winget-pkgs/manifests').rglob('*.en-US.yaml')
+
+package_map = dict()
+
+
+for filepath in all_packages:
+    with open(filepath, 'r') as file:
+        pkg = yaml.safe_load(file)
+    print(pkg)
+    pkg_id = pkg['PackageIdentifier']
+    if pkg_id in package_map:
+        old_pkg = package_map[pkg_id]
+        old_pkg_ver = str(old_pkg['PackageVersion'])
+        pkg_ver = str(pkg['PackageVersion'])
+        if parse_version(pkg_ver) > parse_version(old_pkg_ver):
+            package_map[pkg_id] = pkg
+    else:
+        package_map[pkg_id] = pkg
+
+with open("packages.json", 'w') as outfile:
+    json.dump(list(package_map.values()), outfile)
